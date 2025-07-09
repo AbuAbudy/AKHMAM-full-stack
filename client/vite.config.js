190da641319266f9373e8d89dd9fe4c0f6656080
AbuAbudy <masfurudat@gmail.com>
2025-07-09 16:07:31 +0300

    Fix VITE_API_URL usage in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': `${import.meta.env.VITE_API_URL}`
-    }
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': env.VITE_API_URL,
+      },
+    },
+  };
 });
